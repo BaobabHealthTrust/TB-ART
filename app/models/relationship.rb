9ff02665515ba2b57f97b3c67a51bd74d52eb4f1
Jeff Rafter <jeff@baobabhealth.org>
2010-03-17 10:23:28 -0700

    Relationship types do not have retired, fix the relationships

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -4,7 +4,7 @@ class Relationship < ActiveRecord::Base
   include Openmrs
   belongs_to :person, :class_name => 'Person', :foreign_key => :person_a, :conditions => {:voided => 0}
   belongs_to :relation, :class_name => 'Person', :foreign_key => :person_b, :conditions => {:voided => 0}
-  belongs_to :type, :class_name => "RelationshipType", :foreign_key => :relationship, :conditions => {:retired => 0}
+  belongs_to :type, :class_name => "RelationshipType", :foreign_key => :relationship # no default scope, should have retired
   named_scope :guardian, :conditions => 'relationship_type.b_is_to_a = "Guardian"', :include => :type
   
   def to_s
