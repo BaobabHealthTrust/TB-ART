6b0dd45f79ec73dca8603bdbabcd43cc89f61fb6
ceekays <kachaleedmond@gmail.com>
2011-01-07 23:54:20 +0200

    Added an action for report selection

--- app/controllers/cohort_tool_controller.rb
+++ app/controllers/cohort_tool_controller.rb
@@ -1,2 +1,12 @@
 class CohortToolController < ApplicationController
+
+  def select
+    @report_type = params[:report_type]
+
+    if @report_type == "in_arv_number_range"
+      @arv_number_start = params[:arv_number_start]
+      @arv_number_end   = params[:arv_number_end]
+    end
+  end
+
 end
