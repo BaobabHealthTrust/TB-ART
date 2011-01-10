8233ed1d4308cac54b6feefae58e2c05395ae8e4
chawanangwa <chawanangwa.lupafya@baobabhealth.org>
2011-01-10 17:48:48 +0200

    added the code for processing the report for patients with multiple start reasons. this code is not dnoe yet as it depend on other sections of the whole program that a still in progress.

--- app/controllers/cohort_tool_controller.rb
+++ app/controllers/cohort_tool_controller.rb
@@ -26,16 +26,40 @@ class CohortToolController < ApplicationController
   end
   
   def prescriptions_without_dispensations
+  
+      #The parameter is used by the mastercard go back to the report
+      @report_quarter = params[:quarter]
+      @report_type = params[:report_type]
+      #########################################################
+       
       date_range = quarter_start_and_end_dates()
       start_date = date_range.to_s.split("split").first
       end_date = date_range.to_s.split("split").last
+      prescriptions_without_dispensations_data = Order.find_by_sql(["select order_id, patient_id, date_created from orders 
+                                                 where not exists (select * from obs where orders.order_id = obs.order_id and obs.concept_id = ?) and  
+                                                 date_created >= ? and date_created <= ?", ConceptName.find_by_name('PILLS DISPENSED').concept_id,
+                                                 start_date , end_date ])
+       @report = []
+       prescriptions_without_dispensations_data.each do |prescription|
+         # @report format::: ID, ARVNumber, National_ID, Visit Date, Prescribed_drug
+         arv_number = PatientIdentifier.find(:first, :select => "identifier", 
+                                               :conditions =>["patient_id = ? and identifier_type = ?", prescription[:patient_id],
+                                               PatientIdentifierType.find_by_name('ARV Number').patient_identifier_type_id])
+         national_id = PatientIdentifier.find(:first, :select => "identifier", 
+                                               :conditions =>["patient_id = ? and identifier_type = ?", prescription[:patient_id],
+                                               PatientIdentifierType.find_by_name('National id').patient_identifier_type_id])
+         drug_name = Drug.find(DrugOrder.find(prescription[:order_id]).drug_inventory_id).name
+         @report << [prescription[:patient_id].to_s, arv_number[:identifier].to_s, national_id[:identifier], 
+                     prescription[:date_created].strftime("%Y-%m-%d %H:%M:%S") , drug_name]
+        end
       render :layout => 'report'
   end
   
   def  dispensations_without_prescriptions
        
-       #These parameters are used by the mastercard go back to the report
+       #The parameter is used by the mastercard go back to the report
        @report_quarter = params[:quarter]
+       @report_type = params[:report_type]
        #########################################################
        
        date_range = quarter_start_and_end_dates()
@@ -43,15 +67,17 @@ class CohortToolController < ApplicationController
        end_date = date_range.to_s.split("split").last
        dispensations_without_prescription_data = Observation.find(:all, :select =>  "person_id, value_drug, date_created", 
                                                               :conditions =>["order_id IS NULL and date_created >= ? and date_created <= ? and
-                                                               concept_id = 2834" ,start_date , end_date])
+                                                               concept_id = ?" ,start_date , end_date, 
+                                                               ConceptName.find_by_name('PILLS DISPENSED').concept_id])
        @report = []
        dispensations_without_prescription_data.each do |dispensation|
        # @report format::: ID, ARVNumber, National_ID, Visit Date, Dispensed_Drug
-       #raise PatientIdentifier.inspect
        arv_number = PatientIdentifier.find(:first, :select => "identifier", 
-                                           :conditions =>["patient_id = ? and identifier_type = ?", dispensation[:person_id], '4'])
+                                           :conditions =>["patient_id = ? and identifier_type = ?", dispensation[:person_id],
+                                           PatientIdentifierType.find_by_name('ARV Number').patient_identifier_type_id])
        national_id = PatientIdentifier.find(:first, :select => "identifier", 
-                                           :conditions =>["patient_id = ? and identifier_type = ?", dispensation[:person_id], '3']) 
+                                           :conditions =>["patient_id = ? and identifier_type = ?", dispensation[:person_id],
+                                           PatientIdentifierType.find_by_name('National id').patient_identifier_type_id]) 
        @report << [dispensation[:person_id].to_s, arv_number[:identifier].to_s, national_id[:identifier], 
                    dispensation[:date_created].strftime("%Y-%m-%d %H:%M:%S") , Drug.find(dispensation[:value_drug]).name]
        end
@@ -59,9 +85,38 @@ class CohortToolController < ApplicationController
   end
   
   def  patients_with_multiple_start_reasons
+  
+       #The parameter is used by the mastercard go back to the report
+       @report_quarter = params[:quarter]
+       @report_type = params[:report_type]
+       #########################################################
+       
        date_range = quarter_start_and_end_dates()
        start_date = date_range.to_s.split("split").first
        end_date = date_range.to_s.split("split").last
+       ####################################################TO DO##############################################################
+      patients_with_multiple_start_reasons_data = Observation.find_by_sql(["select person_id, concept_id, date_created, value_coded_name_id from obs
+                                                 where (select COUNT(*) from obs k where k.concept_id = ? and k.person_id = obs.person_id) >= 1 and  
+                                                 date_created >= ? and date_created <= ? and obs.concept_id = ?", 
+                                                 ConceptName.find_by_name('REASON FOR ART ELIGIBILITY').concept_id, start_date , end_date,
+                                                 ConceptName.find_by_name('REASON FOR ART ELIGIBILITY').concept_id ])
+       @report = []
+       national_id = {}
+       arv_number = {}
+       
+       x = 0
+       patients_with_multiple_start_reasons_data.each do |reason|
+         # @report format::: ID, ARVNumber, National_ID, Visit Date, Prescribed_drug
+         arv_number = PatientIdentifier.find(:first, :select => "identifier", 
+                                               :conditions =>["patient_id = ? and identifier_type = ?", reason[:person_id],
+                                               PatientIdentifierType.find_by_name('ARV Number').patient_identifier_type_id]) || {}
+         national_id = PatientIdentifier.find(:first, :select => "identifier", 
+                                               :conditions =>["patient_id = ? and identifier_type = ?", reason[:person_id],
+                                               PatientIdentifierType.find_by_name('National id').patient_identifier_type_id]) || {}
+         @report << [reason[:person_id].to_s, arv_number[:identifier], national_id[:identifier], 
+                     reason[:date_created].strftime("%Y-%m-%d %H:%M:%S") , ConceptName.find(reason[:value_coded_name_id]).name]
+        end
+       #############################################TO DO######################################################################
         render :layout => 'report'
   end
   
@@ -116,7 +171,6 @@ class CohortToolController < ApplicationController
   def last_day_of_month(year, month_number)
         (Date.new(year,12,31).to_date<<(12-month_number)).day
   end
-  
   
 end
 
