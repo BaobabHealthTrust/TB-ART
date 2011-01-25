98ff4c47c654911b30b6bd5152a6376059721511
chawanangwa <chawanangwa.lupafya@baobabhealth.org>
2011-01-25 17:31:30 +0200

    added date filters

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -167,7 +167,7 @@ class Patient < ActiveRecord::Base
     "#{self.person.name}"
   end
 
- def self.dead_with_visits
+ def self.dead_with_visits(start_date, end_date)
 
   patient_died_concept = ConceptName.find_by_name('PATIENT DIED').concept_id
 
@@ -177,7 +177,8 @@ class Patient < ActiveRecord::Base
     ON   dead_state.patient_program_id = dead_patient_program.patient_program_id
     WHERE  EXISTS
       (SELECT * FROM program_workflow_state p
-        WHERE dead_state.state = program_workflow_state_id AND concept_id = #{patient_died_concept}) "
+        WHERE dead_state.state = program_workflow_state_id AND concept_id = #{patient_died_concept})
+          AND obs.obs_datetime >=#{start_date} AND obs.obs_datetime <= #{end_date}"
 
   living_patients = "SELECT living_patient_program.patient_program_id,
     living_state.state, living_patient_program.patient_id, living_state.date_changed
@@ -186,12 +187,14 @@ class Patient < ActiveRecord::Base
     ON living_state.patient_program_id = living_patient_program.patient_program_id
     WHERE  NOT EXISTS
       (SELECT * FROM program_workflow_state p
-        WHERE living_state.state = program_workflow_state_id AND concept_id =  #{patient_died_concept})"
+        WHERE living_state.state = program_workflow_state_id AND concept_id =  #{patient_died_concept})
+          AND obs.obs_datetime >=#{start_date} AND obs.obs_datetime <= #{end_date}"
 
   dead_patients_with_observations_visits = "SELECT s.person_id,s.obs_datetime AS date_dealth, c.obs_datetime AS date_living
     FROM obs c INNER JOIN obs s
     ON s.person_id = c.person_id
-    WHERE s.concept_id != c.concept_id AND s.concept_id =  #{patient_died_concept} AND s.obs_datetime < c.obs_datetime"                                      
+    WHERE s.concept_id != c.concept_id AND s.concept_id =  #{patient_died_concept} AND s.obs_datetime < c.obs_datetime
+      AND obs.obs_datetime >=#{start_date} AND obs.obs_datetime <= #{end_date}"
 
   all_dead_patients_with_visits = " SELECT dead.patient_id, dead.date_changed AS dealth_date, living.date_changed
     FROM (#{dead_patients}) dead,  (#{living_patients}) living
