00d85aae2f1e7cceb3fc88bfcc0a0b1910a158eb
mwatha <mwatha.bwanalie@baobabhealth.org>
2011-02-15 11:55:28 +0200

    adding functionality to do back dat entry

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
-MATEME_VERSION = '2.0 beta'
+BART_VERSION = '2.0 beta'
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -13,12 +13,12 @@ Rails::Initializer.run do |config|
   # config.time_zone = 'UTC'
   
   config.action_controller.session = {
-    :session_key => 'mateme_session',
+    :session_key => 'bart_session',
     :secret      => '8sgdhr431ba87cfd9bea177ba3d344a67acb0f179753f37d28db8bd102134261cdb4b1dbacccb126435631686d66e148a203fac1c5d71eea6abf955a66a472ce'
   }  
 end
 
-MATEME_SETTINGS = YAML.load_file(File.join(Rails.root, "config", "settings.yml"))[Rails.env] rescue nil
+BART_SETTINGS = YAML.load_file(File.join(Rails.root, "config", "settings.yml"))[Rails.env] rescue nil
 
 require 'fixtures'
 require 'composite_primary_keys'
