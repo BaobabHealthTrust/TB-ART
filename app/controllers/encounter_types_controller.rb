85731d9f8fee622ed050022d18aa13241a6b450d
gamphani <dave.phiri@baobabhealth.org>
2011-07-08 10:01:20 +0200

    Reverted to using available encounter files when getting encounters

--- app/controllers/encounter_types_controller.rb
+++ app/controllers/encounter_types_controller.rb
@@ -2,10 +2,10 @@ class EncounterTypesController < ApplicationController
 
   def index
     # TODO add clever sorting
-    #@encounter_types = EncounterType.find(:all).map{|enc|enc.name.gsub(/.*\//,"").gsub(/\..*/,"").humanize}
-    #@available_encounter_types = Dir.glob(RAILS_ROOT+"/app/views/encounters/*.rhtml").map{|file|file.gsub(/.*\//,"").gsub(/\..*/,"").humanize}
-    #@available_encounter_types -= @available_encounter_types - @encounter_types
-    @available_encounter_types = GlobalProperty.find_by_property("statistics.show_encounter_types").property_value.split(',') rescue EncounterType.find(:all).map{|enc|enc.name.gsub(/.*\//,"").gsub(/\..*/,"").humanize}
+    @encounter_types = EncounterType.find(:all).map{|enc|enc.name.gsub(/.*\//,"").gsub(/\..*/,"").humanize}
+    @available_encounter_types = Dir.glob(RAILS_ROOT+"/app/views/encounters/*.rhtml").map{|file|file.gsub(/.*\//,"").gsub(/\..*/,"").humanize}
+    @available_encounter_types -= @available_encounter_types - @encounter_types
+    #@available_encounter_types = GlobalProperty.find_by_property("statistics.show_encounter_types").property_value.split(',') rescue EncounterType.find(:all).map{|enc|enc.name.gsub(/.*\//,"").gsub(/\..*/,"").humanize}
   end
 
   def show
