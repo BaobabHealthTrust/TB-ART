65ca8349a572275c720b6486cd4fe7500a2a9aaf
gamphani <dave.phiri@baobabhealth.org>
2011-07-18 15:54:37 +0200

    Filter search from beginning of the word and interchange l and r where necessary

--- app/controllers/person_addresses_controller.rb
+++ app/controllers/person_addresses_controller.rb
@@ -35,23 +35,25 @@ class PersonAddressesController < ApplicationController
   end
   
   def search_location(type, search_string)
-    
+    rsearch = search_string.gsub('r','l')
+    lsearch = search_string.gsub('l','r')
+  
     @results = []
     if type == 'residence'
       @areas = Location.areas.grep(/#{search_string}/i).compact.sort_by{|area| area.split(" ")[1].to_i}[0..60]
-      @results = @areas + Location.current_residences.grep(/#{search_string}/i).compact.sort_by{|location|
+      @results = @areas + Location.current_residences.grep(/^#{search_string}|^#{rsearch}|^#{lsearch}/i).compact.sort_by{|location|
         location.index(/#{search_string}/) || 100 # if the search string isn't found use value 100
       }[0..10]
     elsif type == 'ta'
-      @results = Location.tas.grep(/#{search_string}/i).compact.sort_by{|ta|
+      @results = Location.tas.grep(/^#{search_string}|^#{rsearch}|^#{lsearch}/i).compact.sort_by{|ta|
           ta.index(/#{search_string}/) || 100 # if the search string isn't found use value 100
         }[0..10]
     elsif type == 'home_vge'
-        @results = Location.current_residences.grep(/#{search_string}/i).compact.sort_by{|location|
+        @results = Location.current_residences.grep(/^#{search_string}|^#{rsearch}|^#{lsearch}/i).compact.sort_by{|location|
           location.index(/#{search_string}/) || 100 # if the search string isn't found use value 100
         }[0..10]
     elsif type == 'health_facility'
-        @results = Location.health_facilities.grep(/#{search_string}/i).compact.sort_by{|facility|
+        @results = Location.health_facilities.grep(/^#{search_string}|^#{rsearch}|^#{lsearch}/i).compact.sort_by{|facility|
           facility.index(/#{search_string}/) || 100 # if the search string isn't found use value 100
         }[0..10]
     end
